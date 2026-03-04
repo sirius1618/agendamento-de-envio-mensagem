729b200fca898ef1d971926d2baffd84512be20e
sirius1618 <sagittarius_a1.618@proton.me>
2026-03-04 16:41:29 -0300

    Inicial: inicializa projeto

---
+++ src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/AgendamentoDeEnvioMensagemApplication.java
@@ -0,0 +1,13 @@
+package com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio_mensagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeEnvioMensagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeEnvioMensagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/AgendamentoDeEnvioMensagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio_mensagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeEnvioMensagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
