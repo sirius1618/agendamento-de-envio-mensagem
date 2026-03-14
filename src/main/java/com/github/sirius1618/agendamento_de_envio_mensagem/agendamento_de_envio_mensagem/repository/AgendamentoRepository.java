c9353891653c6d7cc1149f6106cd26f4e2478dc5
sirius1618 <sagittarius_a1.618@proton.me>
2026-03-13 23:19:18 -0300

    feat: add repository

--- src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/repository/AgendamentoRepository.java
+++ src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/repository/AgendamentoRepository.java
@@ -3,5 +3,5 @@ package com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio
 import com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio_mensagem.model.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface agendamentoRepository extends JpaRepository <Agendamento, Long> {
+public interface AgendamentoRepository extends JpaRepository <Agendamento, Long> {
 }
