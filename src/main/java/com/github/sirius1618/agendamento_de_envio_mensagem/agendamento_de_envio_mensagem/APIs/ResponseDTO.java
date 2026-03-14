addcfba1415b644801b306216ee165e2e9632c1d
sirius1618 <sagittarius_a1.618@proton.me>
2026-03-13 23:21:24 -0300

    feat: add DTO da requisição

--- src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/APIs/ResponseDTO.java
+++ src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/APIs/ResponseDTO.java
@@ -1,4 +1,11 @@
 package com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio_mensagem.APIs;
 
-public record ResponseDTO() {
-}
+import com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio_mensagem.model.TipoMensagem;
+
+import java.time.OffsetDateTime;
+
+public record ResponseDTO(
+        OffsetDateTime dataAgendamento,
+        TipoMensagem tipoMensagem,
+        String mensagem
+) { }
