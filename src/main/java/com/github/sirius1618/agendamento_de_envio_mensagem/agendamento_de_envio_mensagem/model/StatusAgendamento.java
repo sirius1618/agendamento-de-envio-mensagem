61165a5f5529448d00d9880fff795b56ca8d44f1
sirius1618 <sagittarius_a1.618@proton.me>
2026-03-13 23:20:49 -0300

    refactor: mudando organização do repositorio

--- src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/model/StatusAgendamento.java
+++ src/main/java/com/github/sirius1618/agendamento_de_envio_mensagem/agendamento_de_envio_mensagem/model/StatusAgendamento.java
@@ -1,4 +1,4 @@
-package model;
+package com.github.sirius1618.agendamento_de_envio_mensagem.agendamento_de_envio_mensagem.model;
 
 public enum StatusAgendamento {
     PENDENTE, CANCELADO, CONCLUIDO
